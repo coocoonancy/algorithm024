3b66155f5f647427052b28b7f963600a3f9a7fa3
coocoonancy <coocoonancy@coocoonncydeMBP.lan>
2021-02-03 23:39:10 +0800

    feat: week02 刷题

---
+++ Week_02/leetcode.js
@@ -0,0 +1,138 @@
+// [412. Fizz Buzz](https://leetcode-cn.com/problems/fizz-buzz/)
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    // 最小公倍数15
+    const arr = [
+        "",
+        "",
+        "Fizz",
+        "",
+        "Buzz",
+        "Fizz",
+        "",
+        "",
+        "Fizz",
+        "Buzz",
+        "",
+        "Fizz",
+        "",
+        "",
+        "FizzBuzz"
+    ];
+    let resArr = [];
+    for (let i = 0; i < n; i++) {
+        const index = i % 15;
+        resArr[i] =  arr[index] || (i + 1) + '';
+    }
+    return resArr;
+    
+    // Array.from知识点
+    // return Array.from({ length: n }, (item, i) => (item = (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') ? item : '' + i);
+
+
+    // 常规方法 性能差
+    // var fizzBuzz = function(n) {
+    //     const arr = [];
+    //     for (let i = 1; i <= n; i++) {
+    //         if (i % 3 === 0 && i % 5 === 0) {
+    //             arr[i - 1] = 'FizzBuzz';
+    //         } else if (i % 3 === 0) {
+    //             arr[i - 1] = 'Fizz';
+    //         } else if (i % 5 === 0) {
+    //             arr[i - 1] = 'Buzz';
+    //         } else {
+    //             arr[i - 1] = i + '';
+    //         }
+    //     }
+    //     return arr;
+    // };
+};
+
+// [70. 爬楼梯](https://leetcode-cn.com/problems/climbing-stairs/)
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n, m = 1, i = 1) {
+    // 尾递归 没看懂
+    if (n === 1 || n === 0) return m;
+    return climbStairs(n - 1, m + i, m);
+};
+
+var climbStairs = function(n) {
+    // 类推
+    if (n === 1 || n === 0) return ;
+    return climbStairs(n - 1) + climbStairs(n - 2);
+};
+
+// [1. 剑指 Offer 30. 包含min函数的栈](https://leetcode-cn.com/problems/bao-han-minhan-shu-de-zhan-lcof/)
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    this.stack = [];
+    this.minstack = [];
+};
+MinStack.prototype.push = function(x) {
+    this.stack.push(x);
+    if (this.minstack.length === 0 || this.minstack[this.minstack.length - 1] >= x) {
+        this.minstack.push(x);
+    }
+}
+MinStack.prototype.pop = function() {
+    let x = this.stack.pop();
+    if (x !== 0 && this.minstack[this.minstack.length - 1] === x) {
+        this.minStack.pop()
+    }
+
+}
+MinStack.prototype.top = function() {
+    return this.stack[this.stac.length - 1];
+}
+MinStack.prototype.min = function() {
+    return this.minstack[this.minstack.length - 1];
+}
+
+// 24. 两两交换链表中的节点 https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+
+};
+// 21. 合并两个有序列表 https://leetcode-cn.com/problems/merge-two-sorted-lists/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    // 递归
+    if (!l1) return l2;
+    if (!l2) return l1;
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l2.next, l1);
+        return l2;
+    }
+};  
---
+++ Week_02/leetcode.md
@@ -0,0 +1,22 @@
+
+1. 算法刷题狂魔1题
+[350. 两个数组的交集 II](https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/)
+[剑指 Offer 59 - I. 滑动窗口的最大值](https://leetcode-cn.com/problems/hua-dong-chuang-kou-de-zui-da-zhi-lcof/)
+[1021. 删除最外层的括号](https://leetcode-cn.com/problems/remove-outermost-parentheses/)
+[412. Fizz Buzz](https://leetcode-cn.com/problems/fizz-buzz/)
+2. 每日3-5题
+3. 本周新题
+
+###### 数组/链表/跳表
+
+1. 数组/链表/跳表的原理和实现
+
+2. 数组/链表/跳表时间复杂度 空间复杂度
+
+3. 运用
+
+
+4. 升级思维 + 空间换时间
+
+
+###### 老题
