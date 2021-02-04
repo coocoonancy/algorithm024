9da73c37b29bff4d6fb2c8861ffc19a911f9f2cb
coocoonancy <coocoonancy@coocoonncydeMBP.lan>
2021-02-05 07:50:50 +0800

    feat: 图

--- Week_02/README.md
+++ Week_02/README.md
@@ -2,3 +2,17 @@
 ###### 哈希表、映射、集合
 [Java Set](https://docs.oracle.com/en/java/javase/12/docs/api/java.base/java/util/Set.html)
 [Java Map](https://docs.oracle.com/en/java/javase/12/docs/api/java.base/java/util/Map.html)
+###### 树、二叉搜索树、二叉树
+- 链表是特殊化的树
+- 树是特殊化的图
+- 树的遍历
+  - 前序遍历
+  - 中序遍历
+  - 后序遍历
+###### 图
+- 无向无权图
+- 无向有权图
+- 有向无权图
+- 有向有权图
+###### 例题
+[589. N叉树的前序遍历](https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/description/)
--- Week_02/leetcode.js
+++ Week_02/leetcode.js
@@ -1,138 +1,26 @@
-// [412. Fizz Buzz](https://leetcode-cn.com/problems/fizz-buzz/)
+// [350. 两个数组的交集 II](https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/)
 /**
- * @param {number} n
- * @return {string[]}
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
  */
-var fizzBuzz = function(n) {
-    // 最小公倍数15
-    const arr = [
-        "",
-        "",
-        "Fizz",
-        "",
-        "Buzz",
-        "Fizz",
-        "",
-        "",
-        "Fizz",
-        "Buzz",
-        "",
-        "Fizz",
-        "",
-        "",
-        "FizzBuzz"
-    ];
-    let resArr = [];
-    for (let i = 0; i < n; i++) {
-        const index = i % 15;
-        resArr[i] =  arr[index] || (i + 1) + '';
+var intersect = function(nums1, nums2) {
+    /*
+        1. hash表
+    */
+    let isminlen = nums1.length >= nums2.length;
+    let arr1 = isminlen ? nums2 : nums1;
+    let arr2 = isminlen ? nums1 : nums2;
+    let res = [];
+    let hashmap = {};
+    for (let i = 0; i < arr1.length; i++) {
+        hashmap[arr1[i]] = hashmap[arr1[i]] ? hashmap[arr1[i]] + 1 : 1;
     }
-    return resArr;
-    
-    // Array.from知识点
-    // return Array.from({ length: n }, (item, i) => (item = (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') ? item : '' + i);
-
-
-    // 常规方法 性能差
-    // var fizzBuzz = function(n) {
-    //     const arr = [];
-    //     for (let i = 1; i <= n; i++) {
-    //         if (i % 3 === 0 && i % 5 === 0) {
-    //             arr[i - 1] = 'FizzBuzz';
-    //         } else if (i % 3 === 0) {
-    //             arr[i - 1] = 'Fizz';
-    //         } else if (i % 5 === 0) {
-    //             arr[i - 1] = 'Buzz';
-    //         } else {
-    //             arr[i - 1] = i + '';
-    //         }
-    //     }
-    //     return arr;
-    // };
-};
-
-// [70. 爬楼梯](https://leetcode-cn.com/problems/climbing-stairs/)
-/**
- * @param {number} n
- * @return {number}
- */
-var climbStairs = function(n, m = 1, i = 1) {
-    // 尾递归 没看懂
-    if (n === 1 || n === 0) return m;
-    return climbStairs(n - 1, m + i, m);
-};
-
-var climbStairs = function(n) {
-    // 类推
-    if (n === 1 || n === 0) return ;
-    return climbStairs(n - 1) + climbStairs(n - 2);
-};
-
-// [1. 剑指 Offer 30. 包含min函数的栈](https://leetcode-cn.com/problems/bao-han-minhan-shu-de-zhan-lcof/)
-/**
- * initialize your data structure here.
- */
-var MinStack = function() {
-    this.stack = [];
-    this.minstack = [];
-};
-MinStack.prototype.push = function(x) {
-    this.stack.push(x);
-    if (this.minstack.length === 0 || this.minstack[this.minstack.length - 1] >= x) {
-        this.minstack.push(x);
+    for (let j = 0; j < arr2.length; j++) {
+        if (hashmap[arr2[j]]) {
+            res.push(arr2[j]);
+            hashmap[arr2[j]]--;
+        }
     }
-}
-MinStack.prototype.pop = function() {
-    let x = this.stack.pop();
-    if (x !== 0 && this.minstack[this.minstack.length - 1] === x) {
-        this.minStack.pop()
-    }
-
-}
-MinStack.prototype.top = function() {
-    return this.stack[this.stac.length - 1];
-}
-MinStack.prototype.min = function() {
-    return this.minstack[this.minstack.length - 1];
-}
-
-// 24. 两两交换链表中的节点 https://leetcode-cn.com/problems/swap-nodes-in-pairs/
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var swapPairs = function(head) {
-
+    return res;
 };
-// 21. 合并两个有序列表 https://leetcode-cn.com/problems/merge-two-sorted-lists/
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var mergeTwoLists = function(l1, l2) {
-    // 递归
-    if (!l1) return l2;
-    if (!l2) return l1;
-    if (l1.val < l2.val) {
-        l1.next = mergeTwoLists(l1.next, l2);
-        return l1;
-    } else {
-        l2.next = mergeTwoLists(l2.next, l1);
-        return l2;
-    }
-};  
